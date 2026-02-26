a9890c4c472be9ce44117d0ad40ba50c0c8e77ef
PinkiePy <59065701+pinkie-py@users.noreply.github.com>
2026-02-26 01:19:38 +0000

    style(all): format

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -1,29 +1,26 @@
-import { INestApplication } from '@nestjs/common';
-import { Test } from '@nestjs/testing';
-import request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { INestApplication } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+import { App } from "supertest/types";
+import { AppModule } from "./../src/app.module";
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe("AppController (e2e)", () => {
+    let app: INestApplication<App>;
 
-  beforeAll(async () => {
-    const moduleFixture = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    beforeAll(async () => {
+        const moduleFixture = await Test.createTestingModule({
+            imports: [AppModule]
+        }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
 
-  afterAll(async () => {
-    await app.close();
-  });
+    afterAll(async () => {
+        await app.close();
+    });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+    it("/ (GET)", () => {
+        return request(app.getHttpServer()).get("/").expect(200).expect("Hello World!");
+    });
 });
