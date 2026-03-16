dd7f2d403fd1fa58ecf9562b7d3a79a9fc3794ea
Minty <59065701+pinkie-py@users.noreply.github.com>
2026-03-16 15:53:13 +0000

    cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
     // Enable CORS so the React frontend can talk to this API
     app.enableCors({
-        origin: "http://localhost:3001", // Explicitly allow your frontend
+        origin: "*", // Explicitly allow your frontend
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         credentials: true
     });
@@ -15,7 +15,6 @@ async function bootstrap() {
     // INCREASE THE PAYLOAD LIMIT TO 50MB
     app.use(json({ limit: "50mb" }));
     app.use(urlencoded({ extended: true, limit: "50mb" }));
-    app.enableCors();
 
     await app.listen(3000);
 }
